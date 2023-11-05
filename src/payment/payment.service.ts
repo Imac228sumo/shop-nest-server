46ced6563ff745dfa492ab7edec41d3f5e91a29d
unknown <valerkaogorodnikov@mail.ru>
2023-11-05 21:17:56 +0300

    Разрешение к

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -6,36 +6,37 @@ import { CheckPaymentDto } from './dto/check-payment.dto';
 @Injectable()
 export class PaymentService {
   async makePayment(makePaymentDto: MakePaymentDto) {
-      try {
-          const { data } = await axios({
-              method: 'POST',
-              url: 'https://api.yookassa.ru/v3/payments',
-              headers: {
-                  "Content-Type": "application/json",
-                  "Idempotence-Key": Date.now(),
-              },
-              auth: {
-                  username: '274812',
-                  password: 'test_q_rl3UtGZpvew8Dzd0GoTEvw2lmdDyCHH9wFBS5Dkx4',
-              },
-              data: {
-                  amount: {
-                    value: makePaymentDto.amount,
-                    currency: 'RUB',
-                  },
-                  capture: true,
-                  confirmation: {
-                    type: 'redirect',
-                    return_url: 'http://localhost:3001/order',
-                  },
-                  description: makePaymentDto.description,
-              },
-          });
+    try {
+      const { data } = await axios({
+        method: 'POST',
+        url: 'https://api.yookassa.ru/v3/payments',
+        headers: {
+          'Content-Type': 'application/json',
+          'Idempotence-Key': Date.now(),
+        },
+        auth: {
+          username: '274812',
+          password: 'test_q_rl3UtGZpvew8Dzd0GoTEvw2lmdDyCHH9wFBS5Dkx4',
+        },
+        data: {
+          amount: {
+            value: makePaymentDto.amount,
+            currency: 'RUB',
+          },
+          capture: true,
+          confirmation: {
+            type: 'redirect',
+            return_url:
+              'https://shop-next-client-production.up.railway.app/order',
+          },
+          description: makePaymentDto.description,
+        },
+      });
 
-          return data;
-      } catch (error) {
-          throw new ForbiddenException(error);
-      }
+      return data;
+    } catch (error) {
+      throw new ForbiddenException(error);
+    }
   }
 
   async checkPayment(checkPaymentDto: CheckPaymentDto) {
